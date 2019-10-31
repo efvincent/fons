e815f92c9abadc2ba24f4c7726f04fbfe135c7de
Eric Vincent <eric@efvincent.com>
2019-10-31 17:20:12 -0400

    build and clean targets

--- build.fsx
+++ build.fsx
@@ -1,16 +1,28 @@
 #r "paket:
 nuget FSharp.Core
+nuget Fake.DotNet.MSBuild
+nuget Fake.IO.Filesystem
 nuget Fake.Core.Target //"
 
 open Fake.Core
+open Fake.DotNet
+open Fake.IO
+open Fake.IO.Globbing.Operators
+
+let buildDir = "./src/bin"
 
 // ** Define Targets
 Target.create "Clean" (fun _ ->
     Trace.log " --- Cleaning Stuff ---"
+    Shell.cleanDir buildDir
 )
 
 Target.create "Build" (fun _ ->
     Trace.log " --- Building the Library --- "
+    !! "src/**/*.fsproj"
+    |> MSBuild.runRelease id buildDir "Build"
+    |> Trace.logItems "AppBuild output:"
+
 )
 
 Target.create "Deploy" (fun _ ->
